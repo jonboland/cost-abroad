80e0997d56f9415f1f0f5597836784c8bd083a89
jonboland <jon.boland@gmail.com>
2020-01-17 08:39:32 +0000

    Filter JSON data

--- create_cost_abroad.py
+++ create_cost_abroad.py
@@ -3,6 +3,7 @@ import requests
 
 url = "http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/prc_ppp_ind"
 
+# Request JSON from eurostat web server.
 food = requests.get(
     url,
     headers={"Accept": "application/json"},
@@ -11,12 +12,14 @@ food = requests.get(
         "sinceTimePeriod": "2018",
         "groupedIndicators": "1",
         "precision": "1",
-        "ppp_cat": ["A010101"],   #, "A0102"  
+        "ppp_cat": "A010101",   # ["A010101", "A0102"] 
             }
     ).json()
 
+# Write JSON to local file.
 with open('food_store.txt', 'w') as outfile:
     json.dump(food, outfile)
+
 
 
 
