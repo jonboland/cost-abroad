c3acc51b9bb97dcbb78cd471d97512bea4cf2c9d
jonboland <jon.boland@gmail.com>
2020-03-18 17:42:45 +0000

    Handle request exceptions

--- create_cost_abroad.py
+++ create_cost_abroad.py
@@ -16,6 +16,7 @@ restaurant_hotel='A0111'
 import json
 import requests
 from filter_cost_abroad import filter_prices
+from pprint import pprint
 
 
 URL = 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/prc_ppp_ind'
@@ -23,27 +24,43 @@ URL = 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/prc_ppp_ind'
 
 def create_price_files(**kwargs):
     """Create price level data files for each category."""
-    for name, value in kwargs.items():
-        price_write(name, value)
+    for name, code in kwargs.items():
+        create_price_file(name, code)
 
 
-def price_write(name, code):
-    """Write raw price data for a category to local file."""
-    prices = price_raw(code)
+def create_price_file(name, code):
+    """Create price level data file for a category."""
+    prices = prices_raw(code)
     filtered_prices = filter_prices(prices)
+    write_prices(name, filtered_prices)
+    return filtered_prices
+
+
+def write_prices(name, filtered_prices):
     with open(f'{name}.txt', 'w') as outfile:
         json.dump(filtered_prices, outfile)
+    # return filtered_prices
 
 
-def price_raw(code):
-    """Request JSON from eurostat server for a category."""
-    prices = requests.get(
-        URL,
-        headers={'Accept': 'application/json'},
-        params={'na_item': 'PLI_EU28',
-                'lastTimePeriod': '1',
-                'precision': '1',
-                'ppp_cat': code,
-        }
-    ).json()
-    return prices
+def prices_raw(code):
+    try:
+        response = requests.get(URL,
+            headers={'Accept': 'application/json'},
+            params={'na_item': 'PLI_EU28',
+                    'lastTimePeriod': '1',
+                    'precision': '1',
+                    'ppp_cat': code,
+                    }
+            )
+        if 'Dataset contains no data' in response.text:
+            print('invalid category reference number provided')
+        elif not response.text:
+            print('there was a problem handling your request')
+        else:
+            return response.json()
+    except requests.ConnectionError:
+        print('failed to connect')
+    else:
+        if not 200 <= response.status_code <= 299:
+            print(f'status code: {response.status_code} outside of 2xx range')
+
